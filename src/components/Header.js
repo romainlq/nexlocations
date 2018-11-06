84ef979291cbc6b6ee87e2c42ca25bd776d575a7
Romain Le Quellec <romain.lequellec-ext@elis.com>
2018-11-06 13:55:59 +0100

    improve appBar display
    - add firstname + lastname once logged in

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1,
+    marginRight: 20
+  },
+  groupRight: {
+    display: 'flex',
+    alignItems: 'center'
+  }
+};
 class Header extends Component {
   constructor(props) {
     super(props);
@@ -9,28 +26,43 @@ class Header extends Component {
   }
 
   _handleButtonClick() {
-    const { isLoggedIn, login, logout } = this.props;
-    !isLoggedIn ? login() : logout();
+    const { user, login, logout } = this.props;
+    !user.isLoggedIn ? login() : logout();
   }
 
   render() {
-    const { isLoggedIn } = this.props;
+    const { user, classes } = this.props;
     return (
-      <AppBar position="static" color="primary">
-        <Toolbar>
-          <Typography variant="h6" color="inherit">
-            NexLocations
-          </Typography>
-          <Typography variant="h6" color="inherit">
-            {isLoggedIn ? 'CONNECTE' : 'PAS CO'}
-          </Typography>
-          <Button onClick={this._handleButtonClick} variant="outlined" color="secondary">
-            {isLoggedIn ? 'Deconnexion' : 'Se connecter'}
-          </Button>
-        </Toolbar>
-      </AppBar>
+      <div className={classes.root}>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <Typography className={classes.grow} variant="h6" color="inherit">
+              NexLocations
+            </Typography>
+
+            <div className={classes.groupRight}>
+              {user.isLoggedIn && (
+                <Typography
+                  className={classes.grow}
+                  variant="overline"
+                  color="inherit"
+                >
+                  {user.firstname} {user.lastname}
+                </Typography>
+              )}
+              <Button
+                onClick={this._handleButtonClick}
+                variant="outlined"
+                color="secondary"
+              >
+                {user.isLoggedIn ? 'Deconnexion' : 'Se connecter'}
+              </Button>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
     );
   }
 }
 
-export default Header;
+export default withStyles(styles)(Header);
