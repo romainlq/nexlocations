5bbd94a06fd0cab9b614c03f2a292ce0a6a99573
Romain Le Quellec <romain.lequellec@epfedu.fr>
2018-11-28 13:39:15 +0100

    skip login and display only Maps for now

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Search from './Search/Search';
 import Result from './Result/Result';
-// import Map from './Map/Map'; // For now, we don't have the company address from backend
+import Map from './Map/Map'; // For now, we don't have the company address from backend
 import { Grid } from '@material-ui/core';
 import LoginContainer from '../containers/LoginContainer';
 
 const Body = ({ isLoggedIn }) => (
   <Grid container direction="column" justify="center" alignItems="stretch">
-    {isLoggedIn ? (
+    {!isLoggedIn ? (
       <>
-        <Search />
-        <Result />
+        {/* <Search /> */}
+        {/* <Result /> */}
+        <Map />
       </>
     ) : (
       <LoginContainer />
