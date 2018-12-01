3643aeb15ffe41dafe992215d6158391631e5f27
Romain Le Quellec <romain.lequellec@epfedu.fr>
2018-12-01 14:21:56 +0100

    change theme colors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import HeaderContainer from './containers/HeaderContainer';
 import BodyContainer from './containers/BodyContainer';
 
-const theme = createMuiTheme();
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#82b5b1',
+      main: '#63a39e',
+      dark: '#45726e',
+      contrastText: '#FFFFFF'
+    },
+    secondary: {
+      light: '#df6354',
+      main: '#d73c2a',
+      dark: '#962a1d',
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
 class App extends Component {
 
   componentDidMount() {
@@ -13,6 +32,7 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
+        <CssBaseline/>
         <HeaderContainer />
         <BodyContainer />
       </MuiThemeProvider>
