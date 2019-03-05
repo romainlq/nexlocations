b5918fe3c3b1cf79438aed05b13a35f6a47337f3
Romain Le Quellec <romain.lequellec@epfedu.fr>
2019-03-05 13:42:33 +0100

    Updates

---
+++ precache-manifest.865765cdbe69c8159a5b4b1168863da3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "307d34122f93c8e40a99",
+    "url": "/nexlocations/static/js/main.307d3412.chunk.js"
+  },
+  {
+    "revision": "59238f47aa7854c0f882",
+    "url": "/nexlocations/static/js/1.59238f47.chunk.js"
+  },
+  {
+    "revision": "5a9e5263b7f03bb87b3b",
+    "url": "/nexlocations/static/js/runtime~main.5a9e5263.js"
+  },
+  {
+    "revision": "33c72282a2aa7ae03787167f1b722abd",
+    "url": "/nexlocations/index.html"
+  }
+];
